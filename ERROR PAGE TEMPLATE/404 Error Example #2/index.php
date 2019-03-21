ddabf20612418f7bf0e29865e69fdacd0ec6ac08
nuruzzamanhimel0 <nuruzzaman.himel147@gmail.com>
2019-03-21 10:02:24 +0600

    error template

---
+++ ERROR PAGE TEMPLATE/404 Error Example #2/index.php
@@ -0,0 +1,26 @@
+<!-- Link:- https://codepen.io/ricardpriet/pen/qVZxNo -->
+<html>
+	<header>
+		<link rel="stylesheet" href="style.css">
+	
+	</header>
+	<body>
+	
+<!-- <h1>404 Error Page #2</h1>
+<p class="zoom-area"><b>CSS</b> animations to make a cool 404 page. </p -->>
+<section class="error-container">
+  <span class="four"><span class="screen-reader-text">4</span></span>
+  <span class="zero"><span class="screen-reader-text">0</span></span>
+  <span class="four"><span class="screen-reader-text">4</span></span>
+</section>
+<div class="link-container">
+  <a target="_blank" href="https://www.silocreativo.com/en/creative-examples-404-error-css/" class="more-link">Visit the original article</a>
+</div>
+	
+ 
+		
+	
+
+    <script type="text/javascript" src="custom.js"></script>
+	</body>
+</html>
---
+++ ERROR PAGE TEMPLATE/Pure CSS 404 Error Page/index.php
@@ -0,0 +1,29 @@
+<!-- Link:- https://codepen.io/sqfreakz/pen/GJRJOY -->
+<html>
+	<header>
+		<link rel="stylesheet" href="style.css">
+	
+	</header>
+	<body>
+	
+
+	
+ <div id="clouds">
+            <div class="cloud x1"></div>
+            <div class="cloud x1_5"></div>
+            <div class="cloud x2"></div>
+            <div class="cloud x3"></div>
+            <div class="cloud x4"></div>
+            <div class="cloud x5"></div>
+        </div>
+        <div class='c'>
+            <div class='_404'>404</div>
+            <hr>
+            <div class='_1'>THE PAGE</div>
+            <div class='_2'>WAS NOT FOUND</div>
+            <a class='btn' href='#'>BACK TO MARS</a>
+        </div>
+		
+			<script type="text/javascript" src="custom.js"></script>
+	</body>
+</html>

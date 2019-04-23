e1f412c8f9a532e1ddc55e9a25f5898434dac735
nuruzzamanhimel0 <nuruzzaman.himel147@gmail.com>
2019-04-23 15:30:35 +0600

    db update

---
+++ JS AND JQuery/Insert Checkbox values in Database using Ajax, Jquery & PHP/DB/checkboxAjx.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Apr 23, 2019 at 11:29 AM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.3.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `checkboxAjx`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `checkbox`
+--
+
+CREATE TABLE `checkbox` (
+  `id` int(11) NOT NULL,
+  `name` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `checkbox`
+--
+
+INSERT INTO `checkbox` (`id`, `name`) VALUES
+(15, 'Apple'),
+(16, 'Apple,IBM'),
+(17, 'Apple,IBM,Google');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `checkbox`
+--
+ALTER TABLE `checkbox`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `checkbox`
+--
+ALTER TABLE `checkbox`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
